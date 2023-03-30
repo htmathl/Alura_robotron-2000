80ed40fec162a472bb7c7499494b418445a9d776
Matheus <matheus.domingues126@gmail.com>
2023-03-30 13:18:48 -0300

    Adicionar atualização de estatísticas

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 const controle = document.querySelectorAll('[data-controle]');
+const estatistica = document.querySelectorAll('[data-estatistica]');
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 
 controle.forEach(e => {
     e.addEventListener("click", event => {
         manipularDados(event.target.dataset.controle, event.target.parentNode);
+        atualizaEstatistica(event.target.dataset.peca);
     });
 });
 
@@ -13,4 +49,10 @@ function manipularDados(operacao, controle) {
         componente.value = parseInt(componente.value) - 1; 
     if(operacao === "+")
         componente.value = parseInt(componente.value) + 1;
+}
+
+function atualizaEstatistica(peca) {
+    estatistica.forEach(el => {
+        el.textContent = parseInt(el.textContent) + pecas[peca][el.dataset.estatistica];
+    });
 }
